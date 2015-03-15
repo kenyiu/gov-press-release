5473c942dfd0fe97ad50aac110383fc6375d6c82
Ken Yiu <pakhin.yiu@gmail.com>
2015-03-15 20:51:02 +0000

    200701-201503

---
+++ pressRelease/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+
+class PressreleaseItem(Item):
+    # define the fields for your item here like:
+    title = Field()
+    url = Field()
+    content = Field()
+    datetime = Field()
---
+++ pressRelease/spiders/url_spider.py
@@ -0,0 +1,50 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import Selector
+from pressRelease.items import *
+from scrapy.http import Request
+import re
+
+class UrlSpider(BaseSpider):
+	name = "url"
+	# allowed_domains = ["http://www.info.gov.hk"]
+
+	def start_requests(self):
+		for year in range(2007, 2016):
+			for month in range(1, 12):
+				for day in range(1, 32):
+					yield self.make_requests_from_url("http://www.info.gov.hk/gia/general/%04d%02d/%02dc.htm" %(year, month, day))
+
+	def parse(self, response):
+		sel = Selector(response)
+		sites = sel.xpath('//ul[@id="prlist"]/li')
+		datetime = ''.join(sel.select('//h2[@id="date"]/text()').extract());
+		datetime = datetime.split('-')
+		datetime = datetime[2]+"-"+datetime[1]+"-"+datetime[0]
+		items = []
+
+		for site in sites:
+			item = PressreleaseItem()
+			url = ''.join(site.xpath('a/@href').extract())
+			title = ''.join(site.xpath('a/text()').extract())
+			# item['title'] = ''.join(site.xpath('a/text()').extract())
+			# item['link'] = ''.join(site.xpath('a/@href').extract())
+			# url = ''.join(site.xpath('a/@href').extract())
+			request = Request(url=url, callback=self.parse_content)
+			request.meta['item'] = item
+			request.meta['url'] = url
+			request.meta['title'] = title
+			request.meta['datetime'] = datetime
+			# request.meta['datetime'] = re.search(r'http:\/\/www.info.gov.hk/gia/general\/([0-9]{4})([0-9]{2})\/([0-9]{2}c.htm', response.url).group(0)
+			yield request
+			
+	def parse_content(self, response):
+		item = response.meta['item']
+		item['title'] = response.meta['title']
+		item['url'] = response.meta['url']
+		item['datetime'] = response.meta['datetime']
+
+		sel = Selector(response)
+
+		item['content'] = ''.join(sel.select('//div[@id="pressrelease"]').extract()).replace(" ", "")
+		
+		yield item
